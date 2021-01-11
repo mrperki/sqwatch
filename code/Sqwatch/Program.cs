f6cd4bf75744a43edf6ad339e121644494a06e9b
Matt Perkins <matt@matt-perkins.com>
2021-01-10 17:47:10 -0800

    commandline implementation

--- code/Sqwatch/Program.cs
+++ code/Sqwatch/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using CommandLine;
+using System;
+using System.Reflection;
 
 namespace Sqwatch
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HelloWorld();
+
+            return Parser.Default.ParseArguments<Parameters>(args)
+                .MapResult(parameters => Run(parameters), _ => 1);
+        }
+
+        static void HelloWorld()
+        {
+            Console.WriteLine($"sqwatch {Assembly.GetExecutingAssembly().GetName().Version}");
+            Console.WriteLine($"Copyright (c) 2021 Matt Perkins");
+            Console.WriteLine();
+        }
+
+        internal static int Run(Parameters parameters)
+        {
+            // actual app logic goes here
+            return 0;
         }
     }
 }
